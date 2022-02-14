4604bd871e286e33d0bddc108024b0bb379f9430
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2022-02-14 18:27:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import math
+
+app = Flask(__name__)
+
+# X = [(A*B)-(A*D)]/(D-C)
+
+
+@app.route("/calculate",methods=["POST"])
+def calcReqUnits():
+
+    reqobj = request.get_json()
+    NOS = reqobj["NOS"]
+    AVS = reqobj["AVS"]
+    CMV = reqobj["CMV"]
+    TA = reqobj["TA"]
+    Req_Units = ((NOS*AVS) - (NOS*TA))/(TA-CMV)
+    return str(Req_Units)
+
+
+if __name__ == '_main_':
+    app.run()
