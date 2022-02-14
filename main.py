4d37649988f95ff5ed2e7ef5a6a6ce4fcf991e68
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2022-02-14 19:19:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import math
 
+
 app = Flask(__name__)
 
 # X = [(A*B)-(A*D)]/(D-C)
@@ -15,7 +16,8 @@ def calcReqUnits():
     CMV = reqobj["CMV"]
     TA = reqobj["TA"]
     Req_Units = ((NOS*AVS) - (NOS*TA))/(TA-CMV)
-    return str(Req_Units)
+    result = {"url":str(Req_Unit)}
+    return result
 
 
 if __name__ == '_main_':
